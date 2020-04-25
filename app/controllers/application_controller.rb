eb5d67a04782f040f1961c329c00ec53585e9d49
Oleg <gricelevich.oleg@gmail.com>
2020-04-25 17:21:48 +0300

    new users by default are viewers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username)}
+		devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username, :role_id)}
 		devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :username, :role_id)}
 	end
 end
